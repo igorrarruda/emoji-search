9210f2718eebbdbce0e5f80cb4239a99f5424449
igorrarruda <igorrarruda@users.noreply.github.com>
2021-06-14 22:45:25 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.47e9d961579d738649b42b7e59bed8b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba253e2592c6223948a26600d62d3ede",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "58dda7fbf29889301749",
+    "url": "/emoji-search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "956b2e7050e90f2026af",
+    "url": "/emoji-search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "58dda7fbf29889301749",
+    "url": "/emoji-search/static/js/2.0e9a7f6d.chunk.js"
+  },
+  {
+    "revision": "de0a1713895ef548baca42cd10aace43",
+    "url": "/emoji-search/static/js/2.0e9a7f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956b2e7050e90f2026af",
+    "url": "/emoji-search/static/js/main.1b9e6b49.chunk.js"
+  },
+  {
+    "revision": "d44d1531576180c634c7",
+    "url": "/emoji-search/static/js/runtime-main.d8cd5ee4.js"
+  }
+]);
